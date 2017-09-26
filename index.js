c7445aaab2ceffdb5781cf544e70e65a74b96d66
youngky hermanto <yhermanto@gmail.com>
2017-09-26 09:30:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ function add(a,b) {
 function substract(a,b) {
   return a - b
 }
+
+function multiply(a,b) {
+  return a * b
+}
+
+function divide(a,b) {
+  return a/b
+}
+
+function inc(n) {
+  
+}
